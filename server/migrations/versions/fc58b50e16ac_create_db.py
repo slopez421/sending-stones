8591062b38144d2331c0dc8418d3ab2c422d6f15
Shania Lopez <slopez@fandm.edu>
2024-07-17 09:47:01 -0400

    empty revision created

---
+++ server/migrations/versions/fc58b50e16ac_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: fc58b50e16ac
+Revises: 
+Create Date: 2024-07-17 09:46:24.663643
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fc58b50e16ac'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
