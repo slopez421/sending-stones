e449845a1ee25b065a12083c794fdaa13d76a341
Shania Lopez <slopez@fandm.edu>
2024-07-18 15:58:57 -0400

    added relationships between models

--- server/models.py
+++ server/models.py
@@ -12,20 +12,29 @@ class User(db.Model, SerializerMixin):
     first_name = db.Column(db.String, nullable=False)
     last_name = db.Column(db.String, nullable=False)
 
+    listings = db.relationship('Listing', backref='user')
+    comments = db.relationship('Comment', backref='user')
+
 class Listing(db.Model, SerializerMixin):
     __tablename__ = 'listings'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
     players_needed = db.Column(db.Integer)
     players_have = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', backref='listings')
+    comments = db.relationship('Comment', backref='listing')
 
 class Comment(db.Model, SerializerMixin):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
+    body = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     listing_id = db.Column(db.Integer, db.ForeignKey('listings.id'))
+
+    user = db.relationship('User', backref='comments')
+    listing = db.relationship('Listing', backref='comments')
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,61 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User, Comment, Listing
+
+fake = Faker()
+
+def make_users():
+    print("Creating users...")
+    users = []
+    for i in range(10):
+        user = User(
+          first_name = fake.first_name(),
+          last_name = fake.last_name(),
+          username = fake.user_name()
+        )
+        users.append(user)
+
+    db.session.add_all(users)
+    db.session.commit()
+
+def make_listings():
+    print("Creating listings...")
+    listings = []
+    for i in range(10):
+        listing = Listing(
+            title = fake.sentence(),
+            body = fake.sentences(),
+            players_needed = randint(1, 6),
+            players_have = randint(1, 6),
+            user_id = randint(1, len(User.query.all()))
+        )
+
+        listings.append(listing)
+    db.session.add_all(listings)
+    db.session.commit()
+
+def make_comments():
+    print("Creating comments...")
+    comments = []
+    for i in range(10):
+        comment = Comment(
+            body = fake.sentence(),
+            user_id = randint(1, len(User.query.all())),
+            listing_id = randint(1, len(User.query.all()))
+        )
+        comments.append(comment)
+    db.session.add_all(comments)
+    db.session.commit
+
+
 
 if __name__ == '__main__':
-    fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        print("Deleting records...")
+        make_users()
+        make_listings()
+        make_comments()
+        print("Complete.")
