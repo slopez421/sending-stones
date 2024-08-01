8a12951ca976a80d9fc7152bbcafed2e149d69bb
Shania Lopez <slopez@fandm.edu>
2024-08-01 17:39:04 -0400

    changed layout to push listings to the top of the page when added

--- client/src/components/ListingsContainer.js
+++ client/src/components/ListingsContainer.js
@@ -3,11 +3,16 @@ import ListingCard from "./ListingCard";
 
 function ListingsContainer({listings, refreshPage, setRefreshPage, user}) {
     return <div>
-        <div className="divider divider-error"><h1 className="card-title">Adventures Awaiting</h1></div>
-        <div className="grid gap-x-5 gap-y-10 grid-cols-4 mt-10">
-        {listings.map((listing) => <ListingCard key={listing.id} listing={listing}  refreshPage={refreshPage} setRefreshPage={setRefreshPage} currentUser={user}/>
+        <div className="divider divider-error">
+            <h1 className="card-title">Adventures Awaiting</h1></div>
+        <div className="flex flex-row flex-wrap-reverse flex-none mt-10">
+        {listings.map((listing) => 
+        <div className="basis-1/4">
+        <ListingCard key={listing.id} listing={listing}  refreshPage={refreshPage} setRefreshPage={setRefreshPage} currentUser={user}/>
+        </div>
         )}
         </div>
+        
 </div>
 }
 
